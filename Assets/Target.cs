ffb5befa50c644a19f5f743893fddd225791fae7
Jay246h <babojeon641@gmail.com>
2024-11-28 23:43:33 +0900

    난이도 상향

--- Assets/Target.cs
+++ Assets/Target.cs
@@ -7,7 +7,20 @@ public class Target : MonoBehaviour
     private float rotateSpeed = 100f;
     private Vector3 rotateAngle = Vector3.forward; 
 
+     private IEnumerator Start()
+    {
+        while (true)
+        {
+            int time = Random.Range(1, 5);
+            yield return new WaitForSeconds(time);
 
+            int speed = Random.Range(10, 300);
+            int dir = Random.Range(0, 2);
+
+            rotateSpeed = speed;
+            rotateAngle = new Vector3(0, 0, dir * 2-1);
+        }
+    }
     void Update()
     {
         transform.Rotate(rotateAngle *  rotateSpeed * Time.deltaTime);
